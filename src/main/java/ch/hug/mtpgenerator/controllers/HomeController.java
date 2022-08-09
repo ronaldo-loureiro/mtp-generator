cd87df3c353d1fb1b101f17f3c72880d901b18eb
Ronaldo Loureiro <ronaldo.loureiropinto@hcuge.ch>
2022-08-09 16:50:57 +0200

    add a search function for the select + route code and form code turned optional

--- src/main/java/ch/hug/mtpgenerator/controllers/HomeController.java
+++ src/main/java/ch/hug/mtpgenerator/controllers/HomeController.java
@@ -2,6 +2,7 @@ package ch.hug.mtpgenerator.controllers;
 
 import ch.hug.mtpgenerator.models.MtpModel;
 import org.husky.common.enums.LanguageCode;
+import org.husky.common.enums.ValueSetEnumInterface;
 import org.husky.emed.ch.enums.PharmaceuticalDoseFormEdqm;
 import org.husky.emed.ch.enums.RegularUnitCodeAmbu;
 import org.springframework.stereotype.Controller;
@@ -10,17 +11,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import org.husky.emed.ch.enums.RouteOfAdministrationAmbu;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String getHome(final Model model) {
+
         MtpModel mtp = new MtpModel();
         model.addAttribute("mtp", new MtpModel());
-        model.addAttribute("routeOfAdministrationAmbu", RouteOfAdministrationAmbu.values());
-        model.addAttribute("regularUnitCodeAmbu", RegularUnitCodeAmbu.values());
-        model.addAttribute("pharmaceuticalDoseFormEdqm", PharmaceuticalDoseFormEdqm.values());
         model.addAttribute("languageCode", mtp.getDocumentLanguageCode());
+
+        Comparator<ValueSetEnumInterface> comparator = Comparator.comparing(e -> e.getDisplayName(mtp.getDocumentLanguageCode()));
+
+        model.addAttribute("routeOfAdministrationAmbu", Arrays.stream(RouteOfAdministrationAmbu.values())
+                .sorted(comparator)
+                .collect(Collectors.toList()));
+
+        model.addAttribute("regularUnitCodeAmbu", Arrays.stream(RegularUnitCodeAmbu.values())
+                .sorted(comparator)
+                .collect(Collectors.toList()));
+
+        model.addAttribute("pharmaceuticalDoseFormEdqm", Arrays.stream(PharmaceuticalDoseFormEdqm.values())
+                .sorted(comparator)
+                .collect(Collectors.toList()));
+
         return "home";
     }
 }
--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -4,8 +4,21 @@ function changeMtpType(radio_name) {
     if (radio_name === "structured_instructions") {
         document.getElementById("structured-instructions-inputs").style.display = "block";
         document.getElementById("narrative-instructions-inputs").style.display = "none";
+
+        setRequiredInput("narrative-instructions-inputs", false);
+        setRequiredInput("structured-instructions-inputs", true);
     } else if (radio_name === "narrative_instructions") {
         document.getElementById("narrative-instructions-inputs").style.display = "block";
         document.getElementById("structured-instructions-inputs").style.display = "none";
+
+        setRequiredInput("narrative-instructions-inputs", true);
+        setRequiredInput("structured-instructions-inputs", false);
     }
+}
+
+function setRequiredInput(containerId, isRequired) {
+    document.getElementById(containerId).querySelectorAll("input, textarea").forEach((input) => {
+        if (!input.hasAttribute("data-optional"))
+            input.toggleAttribute("required", isRequired);
+    });
 }
